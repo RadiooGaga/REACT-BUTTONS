35f683da1e85e7dc1a3cf7cc600dfe227123a133
RadiooGaga <mariahkatt@gmail.com>
2024-06-05 15:39:04 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ShowCount from './components/ShowCount'
+
+function App() {
+  
+  return (
+      <div id='App'>
+        <ShowCount number={0}/>
+      </div>
+    )
+
+}
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+//LIBRERÍAS DE REACT
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+import './index.css'
+
+const appElement = document.getElementById('root');
+const root = ReactDOM.createRoot(appElement);
+
+
+root.render(<App />)
+
+
+/* ReactDom es la librería, un método de la librería es createRoot, al que le pasas
+un elemento del dom el cual renderiza App, que es el componente.
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />); */
