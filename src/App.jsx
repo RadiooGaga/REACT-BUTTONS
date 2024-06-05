e534edbd4a3794d3b3f0b29f447a78dc42f41d11
RadiooGaga <mariahkatt@gmail.com>
2024-06-05 16:21:38 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import './App.css'
-import ShowCount from './components/ShowCount'
+import { useState } from 'react';
+import ShowCount from './components/ShowCount';
+import './App.css';
+
 
 function App() {
+   /* 0 como valor de inicio y setNumber para cada actualización independiente.
+    color de inicio y setColor para cada actualización del color.*/
+    const [number, setNumber] = useState(0);
+    const [color, setColor] = useState('#252525');
+
+    const updateColor = () => {
+      const newColor = randomColor();
+      setColor(newColor);
+    }
+
+    /* Creamos los botones y asignamos variables, haciendo llamada onClick a las funciones que 
+     realiza cada botón */
+    const buttonSubtract = <button id="subtractButton" onClick={() => {subtract(number, setNumber); updateColor();}}>RESTAR</button>
+    const buttonAdd = <button id="addButton" onClick={() => {add(number, setNumber); updateColor();}}>SUMAR</button>;
   
   return (
       <div id='App'>
-        <ShowCount number={0}/>
+         <ShowCount number={number} color={color} />
+        <div id="divButtons">
+        { buttonSubtract }
+        { buttonAdd }
+        </div>
       </div>
     )
-
 }
+
 export default App
+
+
+
+const randomColor = () => {
+  const red = Math.floor(Math.random() * 156);
+  const green = Math.floor(Math.random() * 156);
+  const blue = Math.floor(Math.random() * 156);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+  
+}
+
+// resta: valor de inicio -1
+const subtract = (prevNumber, setNumber) => {
+    setNumber(prevNumber => prevNumber - 1);
+    randomColor()
+};
+
+// suma: valor de inicio +1
+const add = () => {
+    setNumber(prevNumber => prevNumber + 1);
+    randomColor()
+};
--- src/components/ShowCount.jsx
+++ src/components/ShowCount.jsx
@@ -1,47 +1,11 @@
 // MI COMPONENTE
-import { useState } from 'react';
 import './ShowCount.css';
 
-const ShowCount = (props) => {   
-    // 0 como valor de inicio y setNumber para cada actualización independiente.
-    // color de inicio y setColor para cada actualización del color.
-    const [number, setNumber] = useState(props.number);
-    const [color, setColor] = useState('#252525');
-
-    const randomColor = () => {
-        const red = Math.floor(Math.random() * 156);
-        const green = Math.floor(Math.random() * 156);
-        const blue = Math.floor(Math.random() * 156);
-        
-        const newColor = `rgb(${red}, ${green}, ${blue})`;
-        setColor(newColor);
-    }
-
-    // resta: valor de inicio -1
-    const subtract = () => {
-        setNumber(prevNumber => prevNumber - 1);
-        randomColor()
-    };
-
-    // suma: valor de inicio +1
-    const add = () => {
-        setNumber(prevNumber => prevNumber + 1);
-        randomColor()
-    };
-
-    // Creamos los botones y asignamos variables, haciendo llamada onClick a las funciones que 
-    // realiza cada botón.
-    const buttonSubtract = <button id="subtractButton" onClick={subtract}>RESTAR</button>;
-    const buttonAdd = <button id="addButton" onClick={add}>SUMAR</button>;
-    
-  //el componente
+const ShowCount = (props, color) => {   
+   
   return (
     <div id="divCounter">
-        <h2> You clicked here <span style={{ color: color }}>{number}</span> times</h2>
-      <div id="divButtons">
-        { buttonSubtract }
-        { buttonAdd }
-      </div>
+        <h2> You clicked here <span style={{ color: color }}>{props.number}</span> times</h2>
     </div>
   )
   
